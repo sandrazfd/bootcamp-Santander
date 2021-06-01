d945cb517ff143cc5f3db86ff165152a0fa5c584
sandrasmel <sandrasmel6@gmail.com>
2021-06-01 11:05:38 -0300

    criação de repositorio codigo backend

---
+++ src/main/java/com/project/bootcamp/exceptions/ExceptionResponse.java
@@ -0,0 +1,17 @@
+package com.project.bootcamp.exceptions;
+
+public class ExceptionResponse {
+
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+
+    public ExceptionResponse(String message) {
+        this.message = message;
+    }
+
+}
